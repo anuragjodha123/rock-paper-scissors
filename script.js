6ec522fe87449b287016f2a76d8ebd6f664b906b
anuragjodha123 <anuragjodha123@gmail.com>
2025-02-10 01:48:18 +0000

    created the playRound function (not so happy with the logic tho)

--- script.js
+++ script.js
@@ -15,5 +15,48 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return parseInt(prompt("Write your choice in the prompt below. \n 1. Rock \n 2. Paper \n 3. Scissors"));
+    return prompt("Rock, paper or scissors?").toLowerCase();
+}
+
+// example: if computer took paper
+//  human took rock
+// if computer took paper and human took
+//  computer win!
+//
+//
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("The game is a draw!")
+        return
+    } 
+
+    if (humanChoice == 'paper') {
+        if (computerChoice == 'rock') {
+            console.log('You win! Paper beats Rock.');
+            humanScore++;
+        } else {
+            console.log('You lose! Scissors beats Rock');
+            computerScore++;
+        }
+    } else if(humanChoice == 'rock') {
+        if (computerChoice == 'scissors') {
+            console.log('You win! Rock beats Scissors');
+            humanScore++;
+        } else {
+            console.log('You lose! Paper beats Rock.');
+            computerScore++;
+        }
+    }  else if(humanChoice == 'scissors') {
+        if (computerChoice == 'paper') {
+            console.log('You win! Scissors beats Paper.');
+            humanScore++;
+        } else {
+            console.log('You lose! Rock beats Scissors.');
+            computerScore++;
+        }
+    }
 }
