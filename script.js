5a1660731584abdbd0fde3d84f06aafa22b325f7
anuragjodha123 <anuragjodha123@gmail.com>
2025-02-16 11:14:01 +0000

    created the loop function to loop through rock, paper and scissors

--- script.js
+++ script.js
@@ -53,3 +53,65 @@ function playGame() {
         console.log('Draw!!')
     }
 }
+
+// The gameplay for the slot machine
+// Type 1:
+// User clicks start
+// The computer picks a random nubmer
+// After the time (let's say 10 seconds) the slot stops at that random number
+
+// Type 2:
+// User hits the stop button
+// The machine stops after some time
+// This some time can be a fixed number, which will make this a skill based game
+// Or could be totally random? or vary slightly after every press
+
+// The logic for the rotation of the slot
+// Create an array of Rock Paper Scissors
+// Switch through each of the elements and show it on the screen
+// Increase the time after each loop of the array (to give the slot slowing effect)
+// The increase in time should not be linear it should be exponential
+// Stopping logic:
+  // if increase in time > some pre defined number
+  // and, the current item is the chosen one by random number
+  // stop the loop
+
+let rpsArr = ['Rock', 'Paper', 'Scissors'];
+
+let computerStartBtn = document.querySelector(".computer-start-btn");
+let computerStopBtn = document.querySelector(".computer-stop-btn");
+let slotScreen = document.querySelector("h1");
+
+function changeSlotScreen(text) {
+  slotScreen.textContent = text;
+}
+
+function loopArr() {
+  let newArr = Array(30).fill(rpsArr).flat();
+  let computerChoice = Math.ceil(Math.random()*3);
+  for (let i = 0; i < computerChoice; i++) {
+    newArr.push(rpsArr[i]);
+  }
+  
+  newArr.push('Finished');
+  // some maths for the time
+  // some other time too much maths here
+
+  // let timeArr = Array(newArr.length).fill(1000);
+  // let timeNewArr = [];
+  // for (let i = 0; i < timeArr.length; i++) {
+  //   timeNewArr.push(Math.ceil(Math.pow(1000, ((i+1)/timeArr.length))));
+  // }
+
+  // for (let i = 0; i < timeNewArr.length; i++) {
+  //   timeArr[i] = timeArr[i] - timeNewArr[timeNewArr.length-(i+1)];
+  // }
+
+  for (let i = 0; i < newArr.length; i++) {
+    let timeoutInterval = Math.pow(5000, i/newArr.length);
+
+    setTimeout(() => changeSlotScreen(newArr[i]), timeoutInterval);
+  }
+}
+
+computerStartBtn.addEventListener('click', loopArr);
